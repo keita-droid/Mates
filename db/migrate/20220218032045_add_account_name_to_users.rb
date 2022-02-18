6cf224362ef62674b3696436246bf60da2ee82c5
keita-droid <b.boykeita.ki@gmail.com>
2022-02-18 12:45:32 +0900

    account_nameカラムの制約を修正

--- db/migrate/20220218032045_add_account_name_to_users.rb
+++ db/migrate/20220218032045_add_account_name_to_users.rb
@@ -1,5 +1,5 @@
 class AddAccountNameToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :account_name, :string, null: false, unique: true
+    add_column :users, :account_name, :string, null: false
   end
 end
