9e51978aed13dbca86ce74fab73a2976ced8e5be
keita-droid <b.boykeita.ki@gmail.com>
2022-02-24 16:32:18 +0900

    formのデザインを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-genres = ["映画", "テレビ", "アニメ・漫画", "ゲーム", "本", "スポーツ", "音楽", "レジャー", "動物", "自然", "ファッション", "美容・健康", "食べもの", "旅行", "テクノロジー", "アート", "その他" ]
+genres = ["未分類", "映画", "テレビ", "アニメ・漫画", "ゲーム", "本", "スポーツ", "音楽", "レジャー", "動物", "自然", "ファッション", "美容・健康", "フード・グルメ", "旅行", "テクノロジー", "アート", "その他" ]
 genres.each do |g|
   Genre.create(name: g)
 end
