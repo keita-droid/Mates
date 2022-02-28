b4bf73d85879ca20b67a4df7cddfe3242f2a20a4
keita-droid <b.boykeita.ki@gmail.com>
2022-02-28 19:03:53 +0900

    編集機能をマイページに設置

--- app/javascript/packs/users/show.js
+++ app/javascript/packs/users/show.js
@@ -10,4 +10,19 @@ document.addEventListener("DOMContentLoaded", () => {
     box.classList.add("hidden");
     veil.classList.add("hidden");
   });
+
+  const editButtons = document.getElementsByClassName("edit-btn");
+  for(let i=0; i < editButtons.length; i++) {
+    editButtons[i].onclick = function(){
+      let editBox = document.getElementById(`form-box-edit-${i}`);
+      const veil2 = document.getElementById("transparent-edit");
+      editBox.classList.remove("hidden");
+      veil2.classList.remove("hidden");
+
+      veil2.addEventListener("click", () => {
+        veil2.classList.add("hidden");
+        editBox.classList.add("hidden");
+      });
+    }
+  }
 });
