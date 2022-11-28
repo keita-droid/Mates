84239555d310f407e7df82d59fe266332e6a6d89
keita-droid <b.boykeita.ki@gmail.com>
2022-11-28 16:16:12 +0900

    よくある質問を作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ require("jquery");
 
 import "./groups/show"
 import "./users/show"
+import "./helps/manuals/show"
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
